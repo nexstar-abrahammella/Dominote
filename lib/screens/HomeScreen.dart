4a280b6d955136798af0cb8bc44a43462360a047
AbrahamMella <abrahamwmella@gmail.com>
2020-08-29 16:12:02 -0400

    created HomeScreen widget

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:domiote/components/TeamPointsBoard.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("DOMINOTE"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[TeamPointsBoard(), TeamPointsBoard()],
+      ),
+    );
   }
 }
