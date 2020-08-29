914a8cc53c134e6c58b2add5e43ac4581f68b752
Abraham Mella <a.mella@nexus.com.do>
2020-08-29 18:45:56 -0400

    Feat: Create a constants and styles dart files
    Task: DN-01

---
+++ lib/utilities/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//colors
+
+const blue700 = Color(0xff3A3E47);
+const blue900 = Color(0xff1D2233);
+const blue300 = Color(0xffA8ADB8);
+const blue200 = Color(0xffEFF2F7);
+const whiteColor = Color(0xffFFFFFF);
