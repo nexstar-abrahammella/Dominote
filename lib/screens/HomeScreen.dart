b66369beb59695cd093a117761074ffac4125d8c
Abraham Mella <a.mella@nexus.com.do>
2020-08-29 19:16:56 -0400

    Feat: Adding the app bar and the background color for the application
    Task: DN-06

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:domiote/components/TeamPointsBoard.dart';
-import 'package:domiote/components/headers_components.dart';
+import '../utilities/styles.dart';
+import '../utilities/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarHeader(),
+      backgroundColor: blue700,
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: false,
+        title: Text(headerTitle, style: headerTextStyle),
+        backgroundColor: blue700,
+      ),
       body: Column(
         children: <Widget>[
+          Row(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
