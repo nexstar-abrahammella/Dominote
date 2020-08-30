005176fcfdac789521def3419f1865a45e09e529
Abraham Mella <a.mella@nexus.com.do>
2020-08-29 22:49:02 -0400

    Feat: Create styles for the game information widget

--- lib/utilities/styles.dart
+++ lib/utilities/styles.dart
@@ -7,3 +7,22 @@ const blue900 = Color(0xff1D2233);
 const blue300 = Color(0xffA8ADB8);
 const blue200 = Color(0xffEFF2F7);
 const whiteColor = Color(0xffFFFFFF);
+
+const headerTextStyle = TextStyle(
+  fontFamily: 'Kanit',
+  fontWeight: FontWeight.w900,
+  fontSize: 25.0,
+);
+
+const gameInformationLabelStyle = TextStyle(
+  fontFamily: 'Monserrat',
+  color: whiteColor,
+  fontSize: 16.0,
+);
+
+const gameInformationBorderDecoration = BoxDecoration(
+  border: Border(
+    top: BorderSide(width: 0.3, color: whiteColor),
+    bottom: BorderSide(width: 0.3, color: whiteColor),
+  ),
+);
