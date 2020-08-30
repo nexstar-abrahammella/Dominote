2df43b8de044c18ed8b963e1aa0c0b9a7166d000
Abraham Mella <a.mella@nexus.com.do>
2020-08-29 22:48:21 -0400

    Feat: Adding the widget for game information on the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:domiote/components/TeamPointsBoard.dart';
 import '../utilities/styles.dart';
 import '../utilities/constants.dart';
-
+import 'package:domiote/components/team_points_board.dart';
+import 'package:domiote/components/game_information.dart';
+import '../utilities/internationalization_constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -17,7 +18,41 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Row(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: gameInformationBorderDecoration,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          GameHeaderInformation(
+                            textLabel: seriesTitle,
+                            numberLabel: seriesNumber,
+                          ),
+                          GameHeaderInformation(
+                            textLabel: pointTitle,
+                            numberLabel: seriesNumber,
+                          ),
+                        ],
+                      ),
+                      RawMaterialButton(
+                        onPressed: () {},
+                        elevation: 2.0,
+                        fillColor: blue300,
+                        padding: EdgeInsets.all(15.0),
+                        shape: CircleBorder(),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
